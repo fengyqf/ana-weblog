e7896a43b3a8b1552adf9af9c5d9b35049f46dc8
fengyqf <fengyqf@gmail.com>
2015-09-29 13:40:02 +0800

    bash-script parameter parse, and join for awk-script

---
+++ client_ip.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+
+
+# client_ip.sh [OPTION]... [FILE]...
+
+# 参数列表
+#   -t      type, 日志文件类型，供选值 iis, apache
+#   -s      separate 字段分隔符 av_FS
+#   -p      pattern, 字段模式 av_FPAT
+#   -f      field, 字段编号位置 av_FIELD_INDEX
+#   -d      debug, 输出调试信息 dbg
+
+dbg=0
+#echo "init OPTIND:" $OPTIND
+while getopts "t:s:p:f:d" arg
+do
+    case $arg in
+        t)
+            av_LOGTYPE=$OPTARG
+            ;;
+        s)
+            av_FS=$OPTARG
+            ;;
+        p)
+            av_FPAT=$OPTARG
+            ;;
+        f)
+            av_FIELD_INDEX=$OPTARG
+            ;;
+        d)
+            dbg=1
+            ;;
+        ?)
+    esac
+done
+
+
+if [ "${dbg}" == "1" ]; then
+    echo "---- debug ---------"
+    echo "av_LOGTYPE:        ["$av_LOGTYPE"]"
+    echo "av_FS:             ["$av_FS"]"
+    echo "av_FPAT:           ["$av_FPAT"]"
+    echo "av_FIELD_INDEX:    ["$av_FIELD_INDEX"]"
+    echo "---- debug done ---------"
+fi
+
+
+#if [ "${av_FS}" == "  " ]; then
+#    echo 'av_FS is blank'
+#else
+#    echo "av_FS not"
+#fi
+
+
+shift $((OPTIND-1))
+
+for file in $@
+do
+    echo "file: " $file
+done
+
+if [ -n "${av_LOGTYPE}" ]; then
+    parameters="-v av_LOGTYPE=\""$av_LOGTYPE"\""
+elif [ -n "${av_FS}" ]; then
+    parameters="-v av_FS=\""$av_FS"\""
+elif [ -n "${av_FPAT}" ]; then
+    parameters="-v av_FPAT=\""$av_FPAT"\""
+elif [ -n "${av_FIELD_INDEX}" ]; then
+    parameters="-v av_FIELD_INDEX=\""$av_FIELD_INDEX"\""
+elif [ -n] ; then
+    pass
+fi
+
+echo $parameters
+
+# awk -f analytics.awk $parameters $file
