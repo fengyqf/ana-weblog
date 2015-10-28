210584b80e2f1f42fc4715f15e2380198dae4de5
fengyqf <fengyqf@gmail.com>
2015-10-28 14:50:05 +0800

    clean bash/init.sh

--- src/bash/init.sh
+++ src/bash/init.sh
@@ -1,8 +1,6 @@
 # client_ip.sh [OPTION]... [FILE]...
 
-# TODO 待清理 -----------
 # 参数列表
-# 这些参数列表，应该是没什么用的，有用的主要是计算分析出文件名；参数格式的，忽略 
 #   -t      type, 日志文件类型，供选值 iis, apache等，具体参看 pretreatment.sh 中的定义
 #   -k      keep 保留临时文件
 #   -m      time format, 日志中日期字段格式, 定义在 fs_function.awk 中 fs_str2time()
@@ -46,11 +44,7 @@ if [ "${dbg}" == "1" ]; then
     echo "---- debug done ---------"
 fi
 
-
-
-
 log_filepath=$@
-
 
 
 if [[ ! -f $log_filepath ]]; then
@@ -119,7 +113,6 @@ fi
 if [ -z $count_interval ]; then
     count_interval=60
 fi
-
 
 
 
